7aea85f63607d193080cb13aeccf5eed94e54352
Paul Vincent Craven <paul@cravenfamily.com>
2019-08-15 09:47:41 -0500

    Work at fixing doc path problems

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,16 +25,30 @@ copyright = '2019, Beefy-Swain'
 author = 'Beefy-Swain'
 
 sys.path.insert(0, os.path.abspath('..'))
+print(f"System Path: {sys.path}")
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+pygments_style = 'sphinx'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
