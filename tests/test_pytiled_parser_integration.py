e64c4f1d0f4365007da27a5016ed6af2173f0fb0
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2020-04-18 02:26:00 -0400

    fix: don't re-define map

--- tests/test_pytiled_parser_integration.py
+++ tests/test_pytiled_parser_integration.py
@@ -18,24 +18,24 @@ def test_map_simple():
 
     # map
     # unsure how to get paths to compare propperly
-    assert str(map.parent_dir) == "test_data"
-    assert map.version == "1.2"
-    assert map.tiled_version == "1.2.3"
-    assert map.orientation == "orthogonal"
-    assert map.render_order == "right-down"
-    assert map.map_size == (8, 6)
-    assert map.tile_size == (32, 32)
-    assert map.infinite == False
-    assert map.next_layer_id == 2
-    assert map.next_object_id == 1
+    assert test_map.parent_dir == TEST_DATA
+    assert test_map.version == "1.2"
+    assert test_map.tiled_version == "1.2.3"
+    assert test_map.orientation == "orthogonal"
+    assert test_map.render_order == "right-down"
+    assert test_map.map_size == (8, 6)
+    assert test_map.tile_size == (32, 32)
+    assert test_map.infinite == False
+    assert test_map.next_layer_id == 2
+    assert test_map.next_object_id == 1
 
     # optional, not for orthogonal maps
-    assert map.hex_side_length == None
-    assert map.stagger_axis == None
-    assert map.stagger_index == None
-    assert map.background_color == None
+    assert test_map.hex_side_length == None
+    assert test_map.stagger_axis == None
+    assert test_map.stagger_index == None
+    assert test_map.background_color == None
 
-    assert map.properties == {
+    assert test_map.properties == {
         "bool property - false": False,
         "bool property - true": True,
         "color property": "#ff49fcff",
@@ -46,26 +46,26 @@ def test_map_simple():
     }
 
     # tileset
-    assert map.tile_sets[1].name == "tile_set_image"
-    assert map.tile_sets[1].max_tile_size == (32, 32)
-    assert map.tile_sets[1].spacing == 1
-    assert map.tile_sets[1].margin == 1
-    assert map.tile_sets[1].tile_count == 48
-    assert map.tile_sets[1].columns == 8
-    assert map.tile_sets[1].tile_offset == None
-    assert map.tile_sets[1].grid == None
-    assert map.tile_sets[1].properties == None
+    assert test_map.tile_sets[1].name == "tile_set_image"
+    assert test_map.tile_sets[1].max_tile_size == (32, 32)
+    assert test_map.tile_sets[1].spacing == 1
+    assert test_map.tile_sets[1].margin == 1
+    assert test_map.tile_sets[1].tile_count == 48
+    assert test_map.tile_sets[1].columns == 8
+    assert test_map.tile_sets[1].tile_offset == None
+    assert test_map.tile_sets[1].grid == None
+    assert test_map.tile_sets[1].properties == None
 
     # unsure how to get paths to compare propperly
-    assert str(map.tile_sets[1].image.source) == ("images/tmw_desert_spacing.png")
-    assert map.tile_sets[1].image.trans == None
-    assert map.tile_sets[1].image.size == (265, 199)
+    assert str(test_map.tile_sets[1].image.source) == ("images/tmw_desert_spacing.png")
+    assert test_map.tile_sets[1].image.trans == None
+    assert test_map.tile_sets[1].image.size == (265, 199)
 
-    assert map.tile_sets[1].terrain_types == None
-    assert map.tile_sets[1].tiles == {}
+    assert test_map.tile_sets[1].terrain_types == None
+    assert test_map.tile_sets[1].tiles == {}
 
     # layers
-    assert map.layers[0].data == [
+    assert test_map.layers[0].data == [
         [1, 2, 3, 4, 5, 6, 7, 8],
         [9, 10, 11, 12, 13, 14, 15, 16],
         [17, 18, 19, 20, 21, 22, 23, 24],
@@ -73,12 +73,12 @@ def test_map_simple():
         [33, 34, 35, 36, 37, 38, 39, 40],
         [41, 42, 43, 44, 45, 46, 47, 48],
     ]
-    assert map.layers[0].id_ == 1
-    assert map.layers[0].name == "Tile Layer 1"
-    assert map.layers[0].offset == None
-    assert map.layers[0].opacity == None
-    assert map.layers[0].properties == None
-    assert map.layers[0].size == (8, 6)
+    assert test_map.layers[0].id_ == 1
+    assert test_map.layers[0].name == "Tile Layer 1"
+    assert test_map.layers[0].offset == None
+    assert test_map.layers[0].opacity == None
+    assert test_map.layers[0].properties == None
+    assert test_map.layers[0].size == (8, 6)
 
 
 @pytest.mark.parametrize(
