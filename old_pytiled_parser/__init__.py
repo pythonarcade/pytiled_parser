1864aa7ab6411c90c402a8d74fc14a60c5bf5c13
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2020-05-30 19:20:23 -0400

    Revert "RESET"
    This reverts commit 4ff013c1f8fed99b40aadfea39f97c66291c0d88.

--- old_pytiled_parser/__init__.py
+++
@@ -1,4 +0,0 @@
-"""init for pytiled_parser"""
-
-from . import objects, utilities
-from .xml_parser import parse_tile_map
--- pytiled_parser/__init__.py
+++ pytiled_parser/__init__.py
@@ -1,1 +1,4 @@
-"""Parse Tiled Maps and Tilesets"""
+"""init for pytiled_parser"""
+
+from . import objects, utilities
+from .xml_parser import parse_tile_map
