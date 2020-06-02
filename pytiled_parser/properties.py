0011efdbc262f3c14e7a540538837cd1eb56bf54
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2020-06-01 21:01:23 -0400

    fix(properties): Darren doesn't know how to use VSCode

--- pytiled_parser/properties.py
+++ pytiled_parser/properties.py
@@ -4,7 +4,6 @@ from typing import Dict, List, NamedTuple, Optional, Union
 from .common_types import Color
 
 Property = Union[int, float, Path, str, bool, Color]
-class 
 
 RawProperties = List[Dict[str, Property]]
 
