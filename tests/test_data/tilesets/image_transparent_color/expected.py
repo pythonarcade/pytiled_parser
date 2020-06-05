b860e88f15f61c7bbd88fc9b6d7cbd0ff191b4ad
Benjamin Kirkbride <benjaminkirkbride@gmail.com>
2020-06-04 21:35:01 -0400

    test:fix(image_transparent_color): darren messed up big tim

--- tests/test_data/tilesets/image_transparent_color/expected.py
+++ tests/test_data/tilesets/image_transparent_color/expected.py
@@ -5,14 +5,14 @@ from pytiled_parser.common_types import Color
 
 EXPECTED = tileset.TileSet(
     columns=8,
-    image=Path(r"..\/..\/maps\/images\/tmw_desert_spacing.png"),
+    image=Path("../../maps/images/tmw_desert_spacing.png"),
     image_height=199,
     image_width=265,
     margin=1,
     spacing=1,
-    name="tile_set_image",
+    name="tileset",
     tile_count=48,
-    tiled_version="1.3.1",
+    tiled_version="1.3.5",
     tile_height=32,
     tile_width=32,
     version=1.2,
