2a5e475c93a7608a57d4657e5557d8472bbb17dd
Darren Eberly <darren.eberly@gmail.com>
2021-02-21 01:03:19 -0500

    Initial docs setup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
 
@@ -10,21 +10,20 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
-
 import os
 import sys
 
-project = "PyTiled Parser"
-copyright = "2019, Beefy-Swain"
-author = "Beefy-Swain"
+sys.path.insert(0, os.path.abspath('../..'))
 
-sys.path.insert(0, os.path.abspath("../.."))
+# -- Project information -----------------------------------------------------
+
+project = 'PyTiled Parser'
+copyright = '2021, Beefy-Swain, Cleptomania'
+author = 'Beefy-Swain, Cleptomania'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.0'
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -32,37 +31,34 @@ sys.path.insert(0, os.path.abspath("../.."))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.todo",
-    "sphinx.ext.coverage",
-    "sphinx.ext.ifconfig",
-    "sphinx.ext.viewcode",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-source_suffix = ".rst"
-
-master_doc = "index"
-
-pygments_style = "sphinx"
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,17 +1,11 @@
 PyTiled Parser
 ==============
 
-.. image:: 10_ladders_and_more.png
-    :width: 50%
+PyTiled Parser is a Python library for parsing `Tiled Map Editor`_ (`.json`) files used to generate maps and levels for 2D games.
 
-PyTiled Parser is a Python Library for parsing
-`Tiled Map Editor`_ (`.tmx`) files used to generate
-maps and levels for 2D top-down or side-scrolling games.
+PyTiled Parser is not tied to any particular graphics library, and can be used with Arcade_, Pyglet_, Pygame_, and more.
 
-PyTiled Parser is not tied to any particular graphics library, and can be used
-with Arcade_, Pyglet_, Pygame_, and more.
-
-API Documentation
+API documentation
 -----------------
 
 .. toctree::
@@ -19,18 +13,8 @@ API Documentation
 
    api
 
-Examples
---------
-
-* `Games using the Arcade library <http://arcade.academy/examples/index.html#tmx-files-tiled-map-editor>`_
-
-.. _Tiled Map Editor: https://www.mapeditor.org/
-.. _Arcade: http://arcade.academy
-.. _Pyglet: https://pyglet.readthedocs.io/en/pyglet-1.3-maintenance/
-.. _Pygame: https://www.pygame.org/news
-
 For More Info
 -------------
 
-* `PyTiled Parser on GitHub <https://github.com/pvcraven/pytiled_parser>`_
+* `PyTiled Parser on Github <https://github.com/beefy-swain/pytiled_parser>`_
 * `PyTiled Parser on PyPi <https://pypi.org/project/pytiled-parser/>`_
