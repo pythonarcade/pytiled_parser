117ef785ecf8d767ec67b822ad092ff237781a9b
Darren Eberly <darren@eber.ly>
2023-02-19 17:36:07 -0500

    Switch to pyproject.toml, and to ruff from flake8

--- pytiled_parser/version.py
+++
@@ -1,3 +0,0 @@
-"""pytiled_parser version"""
-
-__version__ = "2.2.1"
--- setup.py
+++
@@ -1,6 +0,0 @@
-# pylint: disable-all
-# type: ignore
-from setuptools import setup
-
-exec(open("pytiled_parser/version.py").read())
-setup(version=__version__)
